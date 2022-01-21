5788e46670b0bf36f1815a66988002fd2e6eb5f0
mmfabian <marcosfabian93@gmail.com>
2022-01-20 23:48:15 -0700

    Create funny.bash.script.sh

---
+++ funny.bash.script.sh
@@ -0,0 +1,38 @@
+#!/bin/bash
+# Marcos J Fabian <marcosfabian93@gmail.com>
+#
+# History 1st draft 01/13/2022
+# Testing BASH scripting. For fun :D
+# 2022 January template
+# Commands used in this script file!
+# clear, echo, sleep, read, while, loop
+clear
+echo
+sleep 1
+echo -e "What is your name?:"
+read name
+sleep 1
+echo "Nice to meet you $name"
+sleep 1
+echo -e "How old are you?:"
+read age
+sleep 1
+echo "I see you are $age... you're kinda old now. I remember when you were this tall!"
+sleep 3
+echo "
+   ,=""=,
+  c , _,{
+  /\  @ )                 __
+ /  ^~~^\          <=.,__/ '}=
+(_/ ,, ,,)          \_ _>_/~
+ ~\_(/-\)'-,_,_,_,-'(_)-(_)"
+sleep 1
+echo -e "How tall are you?"
+read height
+echo "Oh wow, you say you're $height but I don't believe you"
+sleep 1
+echo "I think you're lying..."
+sleep 2
+echo -e "Why should I belive you??"
+read answer
+echo "$answer , okay whatever you say big man ;)"
